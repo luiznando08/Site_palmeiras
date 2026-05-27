49da5c09f7af6015db6927783034379be01c1e74
luiznando08 <Luiznando600@gmail.com>
2026-05-27 01:02:03 -0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,29 @@
+//Obtém o botão do formulário
+var botaoEnviar = document.querySelector("#enviar");
+
+botaoEnviar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    //Captura o formulário
+    var form = document.querySelector("#frmContato");
+
+    //Verifica se o usuário preencheu e-mail ou telefone
+    if(form.nullValue == '' && form.telefone.value==''){
+        return alert("Por favor, preencha um e-mail e/ou um telefone para que possa retornar o seu contato.");
+    } else {
+
+        //Monta o texto de retorno
+        msgRetorno = form.nomesobrenome.value + ", agradecemos o seu contato! <br><br>Vamos avaliar suas considerações e retornaremos.";
+
+        //Abre a div de retorno
+        document.getElementById("formRetorno").style.display="block";
+
+        //Efetua o retorno
+        document.getElementById("textoRetorno").innerHTML=msgRetorno;
+
+        //Fecha o formulário
+        document.getElementById("formulario").style.display="none";
+
+        return;
+    }
+});
